b8e4b1815a144e3dd061ad0437f2ab532a7017b2
Alexandre <alexandreaddres@gmail.com>
2022-02-10 01:22:15 -0300

    V1.0 ready | Game finished

--- app.py
+++ app.py
@@ -1,39 +1,60 @@
 import random
-print('Welcome to Pygessing!')
 
-numberRight = random.randrange(1, 101)
 points = 1000
 numberKick = 0
+numberKicks = 0
 lifes = 0
+
+print('Welcome to Pygessing!')
+
 difficultChoice = int(
-    input('Please, chooise a difficult: (1) Easy - 20 Lifes | (2) Medium - 10 Lifes | (3) Hard | 3 Lifes'))
+    input('Please, chooise a difficult: (1) Easy - 20 Lifes | (2) Medium - 10 Lifes | (3) Hard | 3 Lifes: '))
 
 while(difficultChoice < 1 or difficultChoice > 3):
-        difficultChoice = int(input('Hi, you insert {}. Please, insert a number between 1 at 3 :)'.format(difficultChoice)))
+        difficultChoice = int(input('Hi, you insert {}. Please, insert a number between 1 at 3: )'.format(difficultChoice)))
         continue
 
 if(difficultChoice == 1):
     lifes = 20
-    print('You choice the difficult {} - Easy! Remember kick numbers between 1 and 100. Sucess!'.format(difficultChoice))
+    print('You choice the difficult {} - Easy! You have {} lifes. Remember kick numbers between 1 and 100. Sucess!'.format(difficultChoice, lifes))
 elif (difficultChoice == 2):
     lifes = 10
-    print('You choice the difficult {} - Medium! Remember kick numbers between 1 and 100. Sucess!'.format(difficultChoice))
+    print('You choice the difficult {} - Medium! You have {} lifes. Remember kick numbers between 1 and 100. Sucess!'.format(difficultChoice, lifes))
 else:
     lifes = 3
-    print('You choice the difficult {} - Hard! Remember kick numbers between 1 and 100. Sucess!'.format(difficultChoice))
+    print('You choice the difficult {} - Hard! You have {} lifes. Remember kick numbers between 1 and 100. Sucess!'.format(difficultChoice, lifes))
 
-numberKick = int(input('Please, kick a number between 1 and 100: '))
-numberKicks = 0
+for numberKicks in range(numberKicks, lifes):
+    numberRight = random.randrange(1, 101) #Is here to make a random number every iteration
+    print('You have {} kicks of {} kicks.'.format(numberKicks, lifes))
+    print('')
+    print('Your points: {}'.format(points))
+    print('')
+    numberKick = int(input('Please, kick a number between 1 and 100: '))
 
-numberKickRight = numberKick == numberRight
-numberkickWrongUp = numberKick > numberRight
+    while(numberKick <1 or numberKick > 100):
+        print('Ops... You insert a number most bigger than 100 or smaller than 1.')
+        numberKick = int(input('Please, kick a number between 1 and 100: '))
+        continue
 
-if(numberKickRight):
-    print('Congrats!')
-elif(numberkickWrongUp):
-    print('Ops, you kick up!', numberRight)
+    numberKickRight = numberKick == numberRight
+    numberkickWrongUp = numberKick > numberRight
+
+    print('')
+
+    if(numberKickRight):
+        points = points + (numberKick - numberRight)
+        print('Congrats!')
+    elif(numberkickWrongUp):
+        points = points - (numberKick - numberRight)
+        print('Ops, you kick up!', numberRight)
+    else:
+        points = points + (numberKick - numberRight)
+        print('Ops, you kick down!', numberRight)
+
+print('')
+
+if(points < 1000):
+    print('The end! What a pit! You finished with {} less points than it started!'.format((1000 - points)))
 else:
-    print('Ops, you kick down!', numberRight)
-
-
-
+    print('Congrats! You finished with {} more than it started!'.format((1000 + points)))
